ede9eccda5f83a7d7778d8efe6f76c61ee9313bb
Jimyeong Kim <kjm021221@icloud.com>
2024-11-19 16:43:10 +0900

    feat : Controller에 Swagger 설명 추가

--- src/main/java/com/example/study/controller/MissionChallengeController.java
+++ src/main/java/com/example/study/controller/MissionChallengeController.java
@@ -3,6 +3,7 @@ package com.example.study.controller;
 import com.example.study.dto.request.MissionRequestDTO;
 import com.example.study.dto.response.MissionResponseDTO;
 import com.example.study.service.command.MissionChallengeService;
+import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -19,9 +20,16 @@ public class MissionChallengeController {
     private final MissionChallengeService missionChallengeService;
 
     @PostMapping
+    @Operation(
+            summary = "미션 도전 API",
+            description = "특정 미션에 회원이 도전하도록 등록합니다."
+    )
     public ResponseEntity<MissionResponseDTO.ChallengeMissionResponseDto> challengeMission(
-            @RequestBody @Valid MissionRequestDTO.ChallengeMissionRequestDto request) {
+            @RequestBody @Valid @io.swagger.v3.oas.annotations.parameters.RequestBody(
+                    description = "미션 도전 요청 DTO"
+            ) MissionRequestDTO.ChallengeMissionRequestDto request) {
         MissionResponseDTO.ChallengeMissionResponseDto response = missionChallengeService.challengeMission(request);
         return ResponseEntity.ok(response);
     }
 }
+
--- src/main/java/com/example/study/controller/StoreController.java
+++ src/main/java/com/example/study/controller/StoreController.java
@@ -3,6 +3,7 @@ package com.example.study.controller;
 import com.example.study.dto.request.StortRequestDTO;
 import com.example.study.dto.response.StoreResponseDTO;
 import com.example.study.service.command.StoreService;
+import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -19,9 +20,17 @@ public class StoreController {
     private final StoreService storeService;
 
     @PostMapping
-    public ResponseEntity<StoreResponseDTO.AddStoreResponseDto> addStore(@RequestBody @Valid StortRequestDTO.AddStoreRequestDto request) {
+    @Operation(
+            summary = "가게 추가 API",
+            description = "새로운 가게를 데이터베이스에 추가합니다."
+    )
+    public ResponseEntity<StoreResponseDTO.AddStoreResponseDto> addStore(
+            @RequestBody @Valid @io.swagger.v3.oas.annotations.parameters.RequestBody(
+                    description = "가게 추가 요청 DTO"
+            ) StortRequestDTO.AddStoreRequestDto request) {
         StoreResponseDTO.AddStoreResponseDto response = storeService.addStore(request);
         return ResponseEntity.ok(response);
     }
 }
+
 
